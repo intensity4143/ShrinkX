cb5e4aa39c2f5ff0e4fe6af0de0e819098887699
Pintu Sharma <ipintu4143@gmail.com>
2026-09-19 19:14:28 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ const urlRoutes = require("./routes/urlRoutes")
 const analyticsRoutes = require("./routes/analyticsRoutes")
 const {connectRedis } = require("./config/redis")
 const {connectProducer} = require("./config/kafka");
+const cors = require("cors");
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173"
+}));
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
@@ -26,9 +31,9 @@ const startServer = async () => {
         await connectRedis();
         await connectProducer();
         
-        app.listen(PORT, ()=>{
-        console.log(`server started at port ${PORT}`)
-        })  
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`server started at port ${PORT}`);
+        }); 
     } 
     catch (error) {
         console.log("Failed to start server!", error) 
